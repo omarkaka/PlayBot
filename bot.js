a63e5adfd444f1406b2f9c95868325b13c0337f8
omarkaka <41745215+omarkaka@users.noreply.github.com>
2018-08-07 14:08:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -289,6 +289,8 @@ client.on('message', message => {
 ***❖ &خواطر***
 ***❖ &حب***
 ***❖ &مريم***
+***❖ &فكك***
+
 ╔[❖════════════❖]╗
     ذي هي اوامر بدائية انشاله راح نضيف مزيد من اوامر
 ╚[❖════════════❖]╝
@@ -299,4 +301,78 @@ client.on('message', message => {
     }
 });
 
+var fkk =[
+        {f:"فكك بسم الله الرحمن الرحيم",k:"ب س م ا ل ل ه ا ل ر ح م ن ا ل ر ح ي م"},
+        {f:"فكك باص",k:"ب ا ص"},
+        {f:"فكك عربة ",k:"ع ر ب ة"},
+        {f:"فكك سيارة",k:"س ي ا ر ة"},
+        {f:"فكك سيرفرنا احلى سيرفر",k:"س ي ر ف ر ن ا ا ح ل ى س ي ر ف ر"},
+        {f:"فكك العنود ",k:"ا ل ع ن و د"},
+        {f:"فكك المستتكعكبتيه",k:"ا ل م س ت ت ك ع ك ب ت ي ه"},
+        {f:"فكك دحوم",k:"د ح و م"},
+        {f:"فكك اونرنا احلى اونر",k:"ا و ن ر ن ا ا ح ل ى ا و ن ر"},
+        {f:"فكك الحياة حلوة",k:"ا ل ح ي ا ة ح ل و ة"},
+        {f:"فكك كازخستان ",k:"ك ا ز خ س ت ا ن"},
+        {f:"لحم الحمام حلال ولحم الحمار حرام ",k:"ل ح م ا ل ح م ا م ح ل ا ل و ل ح م ا ل ح م ا ر ح ر ا م"},
+        {f:"فكك استونيا ",k:"ا س ت و ن ي ا"},
+        {f:"فكك لقمة وجغمه ",k:"ل ق م ة و ج غ م ه"},
+        {f:"فكك زنديق  ",k:"ز ن د ي ق"},
+        {f:"فكك استراليا ",k:"ا س ت ر ا ل ي ا"},
+        {f:"فكك سوريا ",k:"س و ر ي ا"},
+        {f:"فكك الاردن ",k:"ا ل ا ر د ن"},
+        {f:"فكك طماطم ",k:"ط م ا ط م"},
+        {f:"فكك سارة ",k:"س ا ر ة"},
+        {f:"فكك دراجون ",k:"د ر ا ج و ن"},
+        {f:"فكك سيرفر ",k:"س ي ر ف ر"},
+        {n:"فكك الجبل",m:"ا ل ج ب ل"},
+        {n:"فكك هضبة",m:"ه ض ب ة"},
+        {n:"فكك خواطر",m:"خ و ا ط ر"},
+        {n:"فكك ارحبو",m:"ا ر ح ب و"},
+        {n:"فكك اطنخ سيرفر",m:"ا ط ن خ س ي ف ر"},
+        {n:"فكك احبك",m:"ا ح ب ك"},
+        {n:"فكك سبرايز",m:"س ب ر ا ي ز"},
+        {n:"فكك ولي على أمتك",m:"و ل ي ع ل ى أ م ت ك"},
+        {n:"فكك الو محد",m:"ا ل و م ح م د"},
+
+
+   ];
+
+
+   client.on("message", async message => {
+    if(message.content == prefix+"فكك"){
+        if(UserBlocked.has(message.guild.id)) return message.channel.send("هناك جلسة .")
+        UserBlocked.add(message.guild.id)
+        var ask = fkk[Math.floor(Math.random() * fkk.length)];
+        let embed = new Discord.RichEmbed()
+        .setTitle('لعبة فكك')
+        .setAuthor(message.author.username, message.author.avatarURL)
+        .setColor("RANDOM")
+        .setDescription(ask.f);
+        message.channel.sendEmbed(embed).then(msg=> msg.delete(200000))
+        const msgs = await message.channel.awaitMessages(msg => msg.author.id !== client.user.id ,{maxMatches:1,time:100000});
+            UserBlocked.delete(message.guild.id)
+        msgs.forEach(result => {
+           if(result.author.id == client.user.id) return;
+           if(result.content == "فكك") return
+           if(result.content == ask.k){
+
+             let embeds = new Discord.RichEmbed()
+             .setTitle(':white_check_mark: اجابة صحيحة')
+             .setAuthor(message.author.username, message.author.avatarURL)
+             .setColor("RANDOM")
+             .setDescription(`**${result.author.username}** الإجابة صحيحة`);
+                message.channel.sendEmbed(embeds);                return;
+           } else {
+
+                               var embedx = new Discord.RichEmbed()
+             .setTitle(':x:خطاء')
+             .setAuthor(message.author.username, message.author.avatarURL)
+             .setColor("RANDOM")
+             .setDescription(`**${result.author.username}** الإجابة خاطئة`);
+
+                message.channel.sendEmbed(embedx);
+           }
+     });
+  }
+});
 client.login(process.env.BOT_TOKEN);
